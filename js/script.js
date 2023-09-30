7c6e0719454a47b4dc4e788e18f645501603ca91
1A291109A6 <128209125+1A291109A6@users.noreply.github.com>
2023-10-01 00:17:58 +0900

    script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 const canvas = document.getElementById('Canvas');
 const context = canvas.getContext('2d');
 
+
+//canvasをウィンドウに合わせる
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+//スクロール不可にする
 function disableScroll() {
   document.body.style.overflow = 'hidden';
-}
+};
 
 disableScroll();
 
 //ゲーム設定
-const FPS = 30; //FPSを指定する
+const FPS = 60; //FPSを指定する
 const Speed = 6; //移動速度
-const rotation_speed = 1; //回転速度
+const rotation_speed = 0.3; //回転速度
 const g = 9.8; //重力加速度
 
 const rotationSpeed = rotation_speed * 2 * Math.PI / FPS;
@@ -27,56 +30,195 @@ var time = performance.now();
 var v_up = 0;
 var map =[];
 
-document.addEventListener('keydown', (event) => {
-  switch(event.key) {
-    case 'ArrowLeft':
-      player[0] -= moveSpeed * Math.cos(player[3]);
-      player[2] += moveSpeed * Math.sin(player[3]);
-      break;
-    case 'ArrowRight':
-      player[0] += moveSpeed * Math.cos(player[3]);
-      player[2] -= moveSpeed * Math.sin(player[3]);
-      break;
-    case 'ArrowUp':
-      player[0] += moveSpeed * Math.sin(player[3]);
-      player[2] += moveSpeed * Math.cos(player[3]);
-      break;
-    case 'ArrowDown':
-      player[0] -= moveSpeed * Math.sin(player[3]);
-      player[2] -= moveSpeed * Math.cos(player[3]);
-      break;
-    case 'a':
+//操作を円滑にできるようにする(毎フレーム処理)
+class KeyControl {
+  constructor() {
+    //キーの状態
+    this.keyStatus = {
+      HOLD: 2,
+      DOWN: 1,
+      UNDOWN: 0,
+      RELEASE: -1,
+    }
+    //nowのキー状態
+    this.input = {
+      keys: {
+        up: false,
+        down: false,
+        right: false,
+        left: false,
+        w: false,
+        a: false,
+        s: false,
+        d: false,
+        q: false,
+        e: false,
+        space: false
+      },
+      //前のフレームのキー状態
+      keysPrev: {
+        up: false,
+        down: false,
+        right: false,
+        left: false,
+        w: false,
+        a: false,
+        s: false,
+        d: false,
+        q: false,
+        e: false,
+        space: false
+      }
+    }
+    //キーが押されたらtrueを返す
+    document.addEventListener('keydown', (e) => {
+      switch(e.key) {
+        case 'ArrowUp':
+          this.input.keys.up = true;
+          break;
+        case 'ArrowDown':
+          this.input.keys.down = true;
+          break;
+        case 'ArrowRight':
+          this.input.keys.right = true;
+          break;
+        case 'ArrowLeft':
+          this.input.keys.left = true;
+          break;
+        case 'w':
+          this.input.keys.w = true;
+          break;
+        case 'a':
+          this.input.keys.a = true;
+          break;
+        case 's':
+          this.input.keys.s = true;
+          break;
+        case 'd':
+          this.input.keys.d = true;
+          break;
+        case 'q':
+          this.input.keys.q = true;
+          break;
+        case 'e':
+          this.input.keys.e = true;
+          break;
+        case ' ':
+          this.input.keys.space = true;
+        }
+    });
+    //キーが離されたらfalseを返す
+    document.addEventListener('keyup', (e) => {
+      switch(e.key) {
+        case 'ArrowUp':
+          this.input.keys.up = false;
+          break;
+        case 'ArrowDown':
+          this.input.keys.down = false;
+          break;
+        case 'ArrowRight':
+          this.input.keys.right = false;
+          break;
+        case 'ArrowLeft':
+          this.input.keys.left = false;
+          break;
+        case 'w':
+          this.input.keys.w = false;
+          break;
+        case 'a':
+          this.input.keys.a = false;
+          break;
+        case 's':
+          this.input.keys.s = false;
+          break;
+        case 'd':
+          this.input.keys.d = false;
+          break;
+        case 'q':
+          this.input.keys.q = false;
+          break;
+        case 'e':
+          this.input.keys.e = false;
+          break;
+        case ' ':
+          this.input.keys.space = false;
+        }
+    });
+  }
+  //キーの状態を確認する
+  checkButton(key) {
+    if(this.input.keys[key]){
+      if(this.input.keysPrev[key] == false){
+        this.input.keysPrev[key] = true;
+        return this.keyStatus.DOWN;
+      }
+      return this.keyStatus.HOLD;
+    }else{
+      if(this.input.keysPrev[key] == true){
+        this.input.keysPrev[key] = false;
+        return this.keyStatus.RELEASE;
+      }
+      return this.keyStatus.UNDOWN;
+    }
+  }
+  
+}
+
+const keyControl = new KeyControl();
+//キー操作
+function control() {
+  //キーと行動の対応
+  const keyActions = {
+    'a': () => {
+      movePlayer(-moveSpeed * Math.cos(player[3]), moveSpeed * Math.sin(player[3]));
+    },
+    'd': () => {
+      movePlayer(moveSpeed * Math.cos(player[3]), -moveSpeed * Math.sin(player[3]));
+    },
+    'w': () => {
+      movePlayer(moveSpeed * Math.sin(player[3]), moveSpeed * Math.cos(player[3]));
+    },
+    's': () => {
+      movePlayer(-moveSpeed * Math.sin(player[3]), -moveSpeed * Math.cos(player[3]));
+    },
+    'left': () => {
       player[3] -= rotationSpeed;
-      break;
-    case 'd':
+    },
+    'right': () => {
       player[3] += rotationSpeed;
-      break;
-    case 's':
-      player[4] -= moveSpeed;
-      if (player[4] < -Math.PI / 2) {
-        player[4] = -Math.PI / 2;
-      }
-      break;
-    case 'w':
-      player[4] += moveSpeed;
-      if (player[4] > Math.PI / 2) {
-        player[4] = Math.PI / 2;
-      }
-      break;
-    case 'q':
+    },
+    'down': () => {
+      player[4] = Math.max(player[4] - rotationSpeed, -Math.PI / 2);
+    },
+    'up': () => {
+      player[4] = Math.min(player[4] + rotationSpeed, Math.PI / 2);
+    },
+    'q': () => {
       player[1] += moveSpeed;
-      break;
-    case 'e':
+    },
+    'e': () => {
       player[1] -= moveSpeed;
-      break;
-    case ' ':
-      if (player[1] == 0) {
+    },
+    'space': () => {
+      if (player[1] === 0) {
         v_up = 4.95;
       }
-      break;
+    },
+  };
+  //行動する
+  function movePlayer(dx, dz) {
+    player[0] += dx;
+    player[2] += dz;
   }
-});
+  //押されたキーから行動を決定する
+  for (const key in keyActions) {
+    if (keyControl.checkButton(key) === keyControl.keyStatus.HOLD) {
+      keyActions[key]();
+    }
+  }
+}
 
+//面を描画
 function draw_plane(ax, ay, bx, by, cx, cy, dx, dy, c) {
   context.beginPath();
   context.moveTo(canvas.width / 2 + ax, canvas.height / 2 - ay);
@@ -88,6 +230,7 @@ function draw_plane(ax, ay, bx, by, cx, cy, dx, dy, c) {
   context.fill();
 }
 
+//三次元の位置データを画面上の座標に変換
 function convert_3Dto2D(x, y, z, cx, cy, cz, rx, ry) {
   if (cx == 0 && cy == 0 && cz == 0) {
     if (rx == 0) {
@@ -105,7 +248,7 @@ function convert_3Dto2D(x, y, z, cx, cy, cz, rx, ry) {
   }
 }
 
-
+//立方体を描画
 function draw_3Dcube(x, y, z, cx, cy, cz, rx, ry, c) {
   point =[];
   convert_3Dto2D(x, y, z, cx, cy, cz, rx, ry);
@@ -136,6 +279,7 @@ function draw_3Dcube(x, y, z, cx, cy, cz, rx, ry, c) {
   }
 }
 
+//重力を考慮
 function c_movement() {
   v_up -= g / FPS;
   player[1] += v_up / FPS;
@@ -146,6 +290,7 @@ function c_movement() {
 
 }
 
+//画面全体を描画する
 function draw() {
   context.fillStyle = '#4169e1';
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -160,6 +305,7 @@ function draw() {
   }
 }
 
+//デバック画面を表示
 function displayCoordinates() {
   context.fillStyle = "rgba(80, 80, 80, 0.8)";
   context.fillRect(0, 0, 280, 50);
@@ -169,24 +315,25 @@ function displayCoordinates() {
   context.fillText(`player(${Math.floor(player[0] * 10) / 10},${Math.floor(player[1] * 10) / 10},${Math.floor(player[2] * 10) / 10})  point(${Math.floor(point[4] * 10) / 10},${Math.floor(point[0] * 10) / 10})`, 10, 20);
 }
 
+//エフェクトをつける
 function draw_effect() {
-  context.strokeStyle = "white"; // 線の色
-  context.lineWidth = 2; // 線の幅
-  context.beginPath(); // パスの開始
-  context.moveTo(canvas.width / 2 + 10, canvas.height / 2); // 開始点の座標 (x, y)
-  context.lineTo(canvas.width / 2 - 10, canvas.height / 2); // 終了点の座標 (x, y)
-  context.stroke(); // 線を描画
-  context.beginPath(); // パスの開始
-  context.moveTo(canvas.width / 2 , canvas.height / 2 + 10); // 開始点の座標 (x, y)
-  context.lineTo(canvas.width / 2 , canvas.height / 2 - 10); // 終了点の座標 (x, y)
-  context.stroke(); // 線を描画
+  //真ん中の十字を描画
+  context.strokeStyle = "white";
+  context.lineWidth = 2;
+  context.beginPath();
+  context.moveTo(canvas.width / 2 + 10, canvas.height / 2);
+  context.lineTo(canvas.width / 2 - 10, canvas.height / 2);
+  context.stroke();
+  context.beginPath();
+  context.moveTo(canvas.width / 2 , canvas.height / 2 + 10);
+  context.lineTo(canvas.width / 2 , canvas.height / 2 - 10);
+  context.stroke();
 }
-
-
 
 setInterval(() => {
   c_movement();
   draw();
+  control();
   draw_effect();
   displayCoordinates();
   time = performance.now();
