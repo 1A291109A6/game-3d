441403d460b49ae4f6ab678bea5358fedb809d04
1A291109A6 <128209125+1A291109A6@users.noreply.github.com>
2023-10-10 00:01:58 +0900

    main.js

--- main.js
+++ main.js
@@ -18,17 +18,57 @@ const FPS = 60; //FPSを指定する
 const Speed = 6; //移動速度
 const rotation_speed = 0.3; //回転速度
 const g = 9.8; //重力加速度
+const FocalLength = 0.5;
 
 const rotationSpeed = rotation_speed * 2 * Math.PI / FPS;
 const moveSpeed = Speed / FPS;
 
 let angle = Math.PI / 2; //視野
 let angle2 = canvas.width / (Math.tan(angle / 2) * 2);
-var player = [0, 0, 0, 0, 0]; //プレイヤーの初期位置
-var point =[];
+var player = [0, 10, 0, 0, 0]; //プレイヤーの初期位置
+var point = [];
 var time = performance.now();
 var v_up = 0;
-var map =[];
+let nowFPS = 60;
+
+function generateRandomArray(num) {
+  const array = new Array(num * num * num);
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      for (let k = 0; k < num; k++) {
+        array[num * num * i + num * j + k] = Math.floor(Math.random() * 6);
+      }
+    }
+  }
+  return array;
+}
+
+//読み込む一辺の数
+var num_block = 5;
+var map = generateRandomArray(num_block);
+
+
+//プレイヤーとの距離が遠い順に並び替える
+function sort_block() {
+  // プレイヤーとの距離の二乗を計算
+  var distance = [];
+  var order = [];
+  for (let i = 0; i < num_block; i++) {
+    for (let j = 0; j < num_block; j++) {
+      for (let k = 0; k < num_block; k++) {
+        let block = num_block * num_block * i + num_block * j + k
+        if (map[block] != 0) {
+          order.push(block)
+          distance.push(Math.pow(player[0] - k - 0.5, 2) + Math.pow(player[1] - j + 1, 2) + Math.pow(player[2] - i - 0.5, 2));
+        }
+      }
+    }
+  }
+  // distance 配列を使用して order 配列をソート
+  order.sort((a, b) => distance[order.indexOf(b)] - distance[order.indexOf(a)]);
+  // order 配列を遠い順に並び替えた結果を格納する新しい配列
+  sortedOrder = order;
+}
 
 //操作を円滑にできるようにする(毎フレーム処理)
 class KeyControl {
@@ -148,13 +188,13 @@ class KeyControl {
   //キーの状態を確認する
   checkButton(key) {
     if(this.input.keys[key]){
-      if(this.input.keysPrev[key] == false){
+      if(this.input.keysPrev[key] == false) {
         this.input.keysPrev[key] = true;
         return this.keyStatus.DOWN;
       }
       return this.keyStatus.HOLD;
     }else{
-      if(this.input.keysPrev[key] == true){
+      if(this.input.keysPrev[key] == true) {
         this.input.keysPrev[key] = false;
         return this.keyStatus.RELEASE;
       }
@@ -218,16 +258,37 @@ function control() {
   }
 }
 
+function set_color(c) {
+  if (c == 1) {
+    context.fillStyle = '#228b22';
+  }
+  if (c == 2) {
+    context.fillStyle = '#006400';
+  }
+  if (c == 3) {
+    context.fillStyle = '#882222';
+  }
+  if (c == 4) {
+    context.fillStyle = '#222288';
+  }
+  if (c == 5) {
+    context.fillStyle = '#640000';
+  }
+}
+
 //面を描画
 function draw_plane(ax, ay, bx, by, cx, cy, dx, dy, c) {
-  context.beginPath();
-  context.moveTo(canvas.width / 2 + ax, canvas.height / 2 - ay);
-  context.lineTo(canvas.width / 2 + bx, canvas.height / 2 - by);
-  context.lineTo(canvas.width / 2 + cx, canvas.height / 2 - cy);
-  context.lineTo(canvas.width / 2 + dx, canvas.height / 2 - dy);
-  context.closePath();
-  context.fillStyle = c;
-  context.fill();
+  //計算不可能領域に頂点があったら描画しない
+  if (ax * bx * cx * dx != 0) {
+    context.beginPath();
+    context.moveTo(canvas.width / 2 + ax, canvas.height / 2 - ay);
+    context.lineTo(canvas.width / 2 + bx, canvas.height / 2 - by);
+    context.lineTo(canvas.width / 2 + cx, canvas.height / 2 - cy);
+    context.lineTo(canvas.width / 2 + dx, canvas.height / 2 - dy);
+    context.closePath();
+    set_color(c);
+    context.fill();
+  }
 }
 
 //三次元の位置データを画面上の座標に変換
@@ -235,8 +296,13 @@ function convert_3Dto2D(x, y, z, cx, cy, cz, rx, ry) {
   if (cx == 0 && cy == 0 && cz == 0) {
     if (rx == 0) {
       if(ry == 0) {
-        point.push(angle2 * x / z);
-        point.push(angle2 * y / z);
+        if (z > FocalLength) {
+          point.push(angle2 * x / z);
+          point.push(angle2 * y / z);
+        }else{
+          point.push(null);
+          point.push(null);
+        }
       }else{
         convert_3Dto2D(x, y * Math.cos(ry) - z * Math.sin(ry), y * Math.sin(ry) + z * Math.cos(ry), 0, 0, 0, 0, 0);
       }
@@ -248,33 +314,35 @@ function convert_3Dto2D(x, y, z, cx, cy, cz, rx, ry) {
   }
 }
 
+
+
 //立方体を描画
 function draw_3Dcube(x, y, z, cx, cy, cz, rx, ry, c) {
   point =[];
-  convert_3Dto2D(x, y, z, cx, cy, cz, rx, ry);
-  convert_3Dto2D(x + 1, y, z, cx, cy, cz, rx, ry);
-  convert_3Dto2D(x, y - 1, z, cx, cy, cz, rx, ry);
-  convert_3Dto2D(x + 1, y - 1, z, cx, cy, cz, rx, ry);
-  convert_3Dto2D(x, y, z + 1, cx, cy, cz, rx, ry);
-  convert_3Dto2D(x + 1, y, z + 1, cx, cy, cz, rx, ry);
-  convert_3Dto2D(x, y - 1, z + 1, cx, cy, cz, rx, ry);
-  convert_3Dto2D(x + 1, y - 1, z + 1, cx, cy, cz, rx, ry);
-  if ((z - cz) > 0) {
+  convert_3Dto2D(x - 0.5, y - 0.5, z - 0.5, cx, cy, cz, rx, ry);
+  convert_3Dto2D(x + 0.5, y - 0.5, z - 0.5, cx, cy, cz, rx, ry);
+  convert_3Dto2D(x - 0.5, y - 1.5, z - 0.5, cx, cy, cz, rx, ry);
+  convert_3Dto2D(x + 0.5, y - 1.5, z - 0.5, cx, cy, cz, rx, ry);
+  convert_3Dto2D(x - 0.5, y - 0.5, z + 0.5, cx, cy, cz, rx, ry);
+  convert_3Dto2D(x + 0.5, y - 0.5, z + 0.5, cx, cy, cz, rx, ry);
+  convert_3Dto2D(x - 0.5, y - 1.5, z + 0.5, cx, cy, cz, rx, ry);
+  convert_3Dto2D(x + 0.5, y - 1.5, z + 0.5, cx, cy, cz, rx, ry);
+  if (z - 0.5 > cz) {
     draw_plane(point[0], point[1], point[2], point[3], point[6], point[7], point[4], point[5], c);
   }
-  if ((x - cx) > 0) {
+  if (x - 0.5 > cx) {
     draw_plane(point[0], point[1], point[4], point[5], point[12], point[13], point[8], point[9], c);
   }
-  if ((cx - x) > 1) {
+  if (cx > x + 0.5) {
     draw_plane(point[2], point[3], point[6], point[7], point[14], point[15], point[10], point[11], c);
   }
-  if ((cy - y) > 0) {
+  if (cy > y - 0.5) {
     draw_plane(point[0], point[1], point[2], point[3], point[10], point[11], point[8], point[9], c);
   }
-  if ((y - cy) > 1) {
+  if (y - 1.5 > cy) {
     draw_plane(point[4], point[5], point[6], point[7], point[14], point[15], point[12], point[13], c);
   }
-  if ((cz - z) > 1) {
+  if (cz > z + 0.5) {
     draw_plane(point[8], point[9], point[10], point[11], point[14], point[15], point[12], point[13], c);
   }
 }
@@ -287,21 +355,15 @@ function c_movement() {
     player[1] = 0;
     v_up = 0;
   }
-
 }
 
 //画面全体を描画する
 function draw() {
   context.fillStyle = '#4169e1';
   context.fillRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i <= 10; i++) {
-    for(let j = 0; j <= 10; j++) {
-      if ((i + j) % 2 == 0) {
-        draw_3Dcube(i, -2, j, player[0], player[1], player[2], player[3], player[4], '#228b22');
-      }else{
-        draw_3Dcube(i, -2, j, player[0], player[1], player[2], player[3], player[4], '#006400');
-      }
-    }
+  sort_block();
+  for (let i = 0; i < sortedOrder.length; i++) { 
+   draw_3Dcube(sortedOrder[i] % num_block, Math.floor(sortedOrder[i] / num_block) % num_block, Math.floor(sortedOrder[i] / (num_block * num_block)), player[0], player[1], player[2], player[3], player[4], map[sortedOrder[i]]);
   }
 }
 
@@ -311,7 +373,8 @@ function displayCoordinates() {
   context.fillRect(0, 0, 280, 50);
   context.fillStyle = 'white';
   context.font = '16px Arial';
-  context.fillText(`FPS: ${Math.floor(1000 / (performance.now() - time))}`, 10, 40);
+  nowFPS = (nowFPS * 10 + 1000 / (performance.now() - time)) / 11
+  context.fillText(`FPS: ${Math.floor(nowFPS)}`, 10, 40);
   context.fillText(`player(${Math.floor(player[0] * 10) / 10},${Math.floor(player[1] * 10) / 10},${Math.floor(player[2] * 10) / 10})  point(${Math.floor(point[4] * 10) / 10},${Math.floor(point[0] * 10) / 10})`, 10, 20);
 }
 
